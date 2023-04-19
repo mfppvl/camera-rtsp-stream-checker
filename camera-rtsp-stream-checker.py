e36e5bddbcd4010244056cc6f20edf624fd4f161
Pavel Asanov <pavelsnv@gmail.com>
2023-04-19 19:53:33 -0400

    Added option to send an email with a report of offline streams.
    Added option to send an email with a report of offline streams upon test completion.
    Email configuration can be set in the config.json file.

--- camera-rtsp-stream-checker.py
+++ camera-rtsp-stream-checker.py
@@ -1,10 +1,15 @@
 import csv
 import cv2
 import datetime
+import smtplib
+import json
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
 from tqdm import tqdm
 
 # Replace the filename with the name of your CSV file
-filename = 'rtsp-url-list.csv'
+filename = 'rtsp.csv'
 
 # Set a timeout value in milliseconds
 timeout_ms = 1500
@@ -20,7 +25,7 @@ with open('stream-down-log.csv', 'w', newline='') as log_file:
     log_writer.writerow(['Timestamp', 'Stream Number', 'DVR Name', 'RTSP URL'])
     
     with open(filename, 'r') as csvfile:
-        print(f"\n File opened")
+        print(f"\n CSV file opened")
         stream_reader = csv.reader(csvfile)
         # Use tqdm to display progress bar
         for row in tqdm(stream_reader):
@@ -45,9 +50,40 @@ with open('stream-down-log.csv', 'w', newline='') as log_file:
             cap.release()
 
 # Print summary of up and down streams
-# print(f"\n{up_count} RTSP streams are up and {down_count} RTSP streams are down.")
 print(f"\n{up_count} RTSP streams are up and \033[1;31m{down_count} RTSP streams are down\033[0m")
 print(f"\n Check Completed!")
+
+# Send the log file via email
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+
+from_address = config['from_address']
+to_address = config['to_address']
+subject = config['subject']
+body = config['body']
+smtp_server = config['smtp_server']
+smtp_port = config['smtp_port']
+smtp_username = config['smtp_username']
+smtp_password = config['smtp_password']
+
+msg = MIMEMultipart()
+msg['From'] = from_address
+msg['To'] = to_address
+msg['Subject'] = subject
+
+msg.attach(MIMEText(body, 'plain'))
+
+with open('stream-down-log.csv', 'rb') as file:
+    attachment = MIMEApplication(file.read(), _subtype='csv')
+    attachment.add_header('Content-Disposition', 'attachment', filename='stream-down-log.csv')
+    msg.attach(attachment)
+
+server = smtplib.SMTP(smtp_server, smtp_port)
+server.starttls()
+server.login(smtp_username, smtp_password)
+server.sendmail(from_address, to_address, msg.as_string())
+server.quit()
 
 
 
